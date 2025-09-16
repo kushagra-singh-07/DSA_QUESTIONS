0ba4b169bb531360f4f561703a16b7a36d9d3379
Kushagra007 <kushagrasinghbora@gmail.com>
2025-09-16 21:26:03 +0530

    Update Spiral_Traversal.cpp

--- Spiral_Traversal.cpp
+++ Spiral_Traversal.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 
 int main() {
-    int n, m;
-    cin >>n>>m;
+    int N, M;
+    cin >>N>>M;
 
     int arr[100][100];
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < m; j++)
+    for(int i = 0; i < N; i++)
+        for(int j = 0; j < M; j++)
             cin >> arr[i][j];
 
-    int top = 0, bottom = n - 1;
-    int left = 0, right = m - 1;
+    int top = 0, bottom = N - 1;
+    int left = 0, right = M - 1;
 
     while(top <= bottom && left <= right) {
        
@@ -36,4 +36,5 @@ int main() {
     }
     return 0;
 }
+
 
